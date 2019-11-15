4cda50720bc0ec611b5a4ae0f9138f5cead2c931
Yoznob <ydbeis@asu.edu>
2019-11-15 15:20:58 -0700

    Home page

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:frontend/pages/start/start.screen.dart';
 import 'package:frontend/pages/login/login.screen.dart';
 import 'package:frontend/pages/home/home.screen.dart';
+import 'package:frontend/pages/message thread/message.list.dart';
 
 Map<String, Widget Function(BuildContext)> routePages = {
   '/': (BuildContext context) => StartScreen(),
+  '/message-list': (BuildContext context) => MessageList(),
   '/create-account': (BuildContext context) => new Scaffold(),
   '/login': (BuildContext context) => LoginScreen(),
   '/home': (BuildContext context) => HomeScreen(),
